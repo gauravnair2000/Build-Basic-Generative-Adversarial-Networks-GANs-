5852418a452a7ac627805da8b982fabfd0025599
Gaurav Nair <gauravnair2000@gmail.com>
2021-05-15 13:57:37 +0530

    Initial commit

---
+++ week1.py
@@ -0,0 +1,71 @@
+import torch
+from torch import nn
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+
+mnist_trainset = MNIST(root='./data', train=True, download=False, transform=None)
+
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in a uniform grid.
+    '''
+    image_unflat = image_tensor.detach().cpu().view(-1, *size)
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+# Generator
+# UNQ_C1 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: get_generator_block
+def get_generator_block(input_dim, output_dim):
+    '''
+    Function for returning a block of the generator's neural network
+    given input and output dimensions.
+    Parameters:
+        input_dim: the dimension of the input vector, a scalar
+        output_dim: the dimension of the output vector, a scalar
+    Returns:
+        a generator neural network layer, with a linear transformation
+          followed by a batch normalization and then a relu activation
+    '''
+    return nn.Sequential(
+        # Hint: Replace all of the "None" with the appropriate dimensions.
+        # The documentation may be useful if you're less familiar with PyTorch:
+        # https://pytorch.org/docs/stable/nn.html.
+        #### START CODE HERE ####
+        nn.Linear(input_dim, output_dim),
+        nn.BatchNorm1d(output_dim),
+        nn.ReLU(inplace=True),
+        #### END CODE HERE ####
+    )
+
+
+# Verify the generator block function
+def test_gen_block(in_features, out_features, num_test=1000):
+    block = get_generator_block(in_features, out_features)
+
+    # Check the three parts
+    assert len(block) == 3
+    assert type(block[0]) == nn.Linear
+    assert type(block[1]) == nn.BatchNorm1d
+    assert type(block[2]) == nn.ReLU
+
+    # Check the output shape
+    test_input = torch.randn(num_test, in_features)
+    test_output = block(test_input)
+    assert tuple(test_output.shape) == (num_test, out_features)
+    assert test_output.std() > 0.55
+    assert test_output.std() < 0.65
+
+
+test_gen_block(25, 12)
+test_gen_block(15, 28)
+print("Success!")
+
+
